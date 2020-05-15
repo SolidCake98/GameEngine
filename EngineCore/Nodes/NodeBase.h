1d5e51cd443bd8519e14720f10c703427628ec33
Valduy <whnpir@gmail.com>
2020-05-15 15:35:48 +0300

    Положил начало нодам (добавил физический нод)

---
+++ EngineCore/Nodes/NodeBase.h
@@ -0,0 +1,14 @@
+//
+// Created by gleb on 15.05.2020.
+//
+
+#ifndef GAMEENGINE_NODEBASE_H
+#define GAMEENGINE_NODEBASE_H
+
+class NodeBase
+{
+protected:
+    NodeBase(){}
+};
+
+#endif //GAMEENGINE_NODEBASE_H
---
+++ EngineCore/Nodes/PhysicsNode.h
@@ -0,0 +1,35 @@
+//
+// Created by gleb on 15.05.2020.
+//
+
+#ifndef GAMEENGINE_PHYSICSNODE_H
+#define GAMEENGINE_PHYSICSNODE_H
+
+#include "NodeBase.h"
+#include "../Components/PositionComponent.h"
+#include "../Components/VelocityComponent.h"
+#include "../Components/BodyComponent.h"
+#include "../Components/ShapeComponent.h"
+
+class PhysicsNode : NodeBase
+{
+public:
+    PhysicsNode(PositionComponent& position,
+                VelocityComponent& velocity,
+                BodyComponent& body,
+                ShapeComponent& shape);
+
+    const PositionComponent& GetPosition();
+    const VelocityComponent& GetVelocity();
+    const BodyComponent& GetBody();
+    const ShapeComponent& GetShape();
+
+private:
+    PositionComponent* _position;
+    VelocityComponent* _velocity;
+    BodyComponent* _body;
+    ShapeComponent* _shape;
+};
+
+
+#endif //GAMEENGINE_PHYSICSNODE_H
