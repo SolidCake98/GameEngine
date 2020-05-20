6190f71f68766e8f0c2aca87a29b01dcc240cc39
Valduy <whnpir@gmail.com>
2020-05-20 17:01:24 +0300

    Добавил немного комментариев

--- EngineCore/Systems/SystemBase.h
+++ EngineCore/Systems/SystemBase.h
@@ -5,13 +5,18 @@
 #ifndef GAMEENGINE_SYSTEMBASE_H
 #define GAMEENGINE_SYSTEMBASE_H
 
-
 #include "../Entity.h"
 
+// Описание:
+// Базовый класс для систем. Система отвечает за тот или иной аспект игровой логики.
 class SystemBase
 {
 public:
+    // Описание:
+    // Метод, который должен приводить сущности в актуальное состояние.
     virtual void Update() = 0;
+    // Описание:
+    // Метод, регестрирующий сущности в системе (как правило, это подразумевает регистрацию необходимых системе узлов)
     virtual void Register(Entity& entity) = 0;
 };
 
